540bd5bc8826d40db81b05fb118260e25f21dc4b
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-10-06 00:22:56 +0200

    Add permissions-policy middleware (WIP)

---
+++ src/middlewares/permissions-policy.js
@@ -0,0 +1,15 @@
+export function permissionsPolicy (options = {}) {
+
+  return async (context) => {
+
+    const featurePolicyHeader = 'accelerometer \'none\'; camera \'none\'; geolocation \'none\'; gyroscope \'none\'; magnetometer \'none\'; microphone \'none\'; payment \'none\'; usb \'none\'';
+    const permissionsPolicyHeader = 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()';
+
+    const responseHeaders = {
+      ...context.responseHeaders,
+      'feature-policy': permissionsPolicyHeader,
+      'permissions-policy': permissionsPolicyHeader,
+    };
+    return { ...context, responseHeaders };
+  };
+}
