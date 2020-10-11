645227097aac62248e4ebd44f2041925f5d66afe
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-10-11 13:05:34 +0200

    permissions-policy: only add header if HTML like

--- src/middlewares/permissions-policy.js
+++ src/middlewares/permissions-policy.js
@@ -1,6 +1,13 @@
+import { isHtml } from '../lib/content-type.js';
+
 export function permissionsPolicy (options = {}) {
 
   return async (context) => {
+
+    const contentTypeHeader = context.responseHeaders['content-type'];
+    if (!isHtml(contentTypeHeader)) {
+      return;
+    }
 
     const featurePolicyHeader = 'accelerometer \'none\'; camera \'none\'; geolocation \'none\'; gyroscope \'none\'; magnetometer \'none\'; microphone \'none\'; payment \'none\'; usb \'none\'';
     const permissionsPolicyHeader = 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()';
