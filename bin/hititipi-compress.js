cb78df239db59559dfc5cb2911d894a8577209bc
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-11-11 18:52:20 +0100

    compress: make zopfli conditionnal and add concurrency

--- bin/hititipi-compress.js
+++ bin/hititipi-compress.js
@@ -6,13 +6,24 @@ import { promisify } from 'util';
 
 const pipeline = promisify(rawPipeline);
 
-const [INPUT_GLOB] = process.argv.slice(2);
-
 async function run () {
-  if (INPUT_GLOB == null) {
+
+  // With a sample of 1500+ HTML/CSS/JS files,
+  // most of the files where the zlib/zopfli diff was under 500 bytes where smaller than 50000/70000 bytes.
+  // A threshold of 60000 bytes for zopfli seems like a good default.
+
+  // Seems to be a good value :p
+  const concurrency = 8;
+
+  const [inputGlob, zopfliThreshold = 60000] = process.argv.slice(2);
+  if (inputGlob == null) {
     throw new Error('hititipi-compress needs the glob of files to compress as the first arg');
   }
-  await compressFiles(INPUT_GLOB);
+
+  console.log(`Starting compression on ${inputGlob}`);
+  console.time('Task finished');
+  await compressFiles(inputGlob, { zopfliThreshold, concurrency });
+  console.timeEnd('Task finished');
 }
 
 run()
