17701da69604615d729e521559cf1546dca4a8be
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-10-09 00:21:35 +0200

    feat: add proxy middleware

---
+++ src/middlewares/proxy/proxy.js
@@ -0,0 +1,40 @@
+import { request as requestWithUndici } from 'undici';
+import { toStandardHeaders } from '../../hititipi.common.js';
+import { readableToWebReadableStream } from '../../lib/node-streams.js';
+import { cloneUrl } from '../redirect/redirect.js';
+
+/**
+ * @typedef {import('../../types/hititipi.types.d.ts').HititipiMiddleware} HititipiMiddleware
+ * @typedef {import('../../types/hititipi.types.d.ts').UrlParts} UrlParts
+ */
+
+/**
+ * @param {UrlParts} urlParts
+ * @return {HititipiMiddleware}
+ */
+export function proxy(urlParts) {
+  return async (context) => {
+    const targetUrl = cloneUrl(context.requestUrl, urlParts);
+
+    const hostHeader = context.requestHeaders.get('host');
+    context.requestHeaders.delete('host');
+
+    const response = await requestWithUndici(targetUrl, {
+      // @ts-ignore
+      method: context.requestMethod,
+      headers: context.requestHeaders,
+    });
+
+    if (hostHeader != null) {
+      context.requestHeaders.set('host', hostHeader);
+    }
+
+    context.responseStatus = response.statusCode;
+    context.responseHeaders = toStandardHeaders(response.headers);
+    if (response.body != null) {
+      context.responseBody = readableToWebReadableStream(response.body);
+    }
+
+    return context;
+  };
+}
---
+++ src/middlewares/proxy/proxy.md
@@ -0,0 +1,13 @@
+# proxy middleware
+
+The `proxy` middleware TODO.
+
+## Import
+
+```js
+import { proxy } from 'hititipi/middlewares/proxy/proxy.js';
+```
+
+## Examples
+
+TODO
