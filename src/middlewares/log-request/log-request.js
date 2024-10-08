a0a5ed1d9c22805de1093d29025635c2e1ddbdaa
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-10-09 00:21:35 +0200

    feat: add log-request middleware

---
+++ src/middlewares/log-request/log-request.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {import('../../types/hititipi.types.d.ts').HititipiMiddleware} HititipiMiddleware
+ * @typedef {import('./log-request.types.d.ts').LogRequestOptions} LogRequestOptions
+ */
+
+/**
+ * @param {LogRequestOptions} options
+ * @return {HititipiMiddleware}
+ */
+export function logRequest(options) {
+  return async (context) => {
+    const params = [
+      options.hideTimestamps ? null : new Date(context.requestTimestamp).toISOString(),
+      `[${context.requestId}]`,
+      context.requestMethod,
+      context.requestUrl.pathname,
+      context.requestUrl.search,
+      context.responseStatus ?? 501,
+      `${Date.now() - context.requestTimestamp}ms`,
+    ].filter((a) => a != null);
+    options.logFunction(...params);
+    return context;
+  };
+}
---
+++ src/middlewares/log-request/log-request.test.js
@@ -0,0 +1,40 @@
+import assert from 'node:assert';
+import { describe, it, mock } from 'node:test';
+import { initTestContext } from '../../lib/init-test-context.js';
+import { logRequest } from './log-request.js';
+
+/**
+ * @param {any} [_message]
+ * @param  {Array<any>} _optionalParams
+ */
+function consoleLogNoop(_message, ..._optionalParams) {}
+
+describe('middleware / log-request', () => {
+  it('hideTimestamps: false', async () => {
+    const context = initTestContext();
+    const logFunction = mock.fn(consoleLogNoop);
+    await logRequest({ hideTimestamps: false, logFunction })(context);
+    assert.equal(logFunction.mock.callCount(), 1);
+    const callArgs = logFunction.mock.calls[0].arguments;
+    assert.equal(callArgs[0], new Date(context.requestTimestamp).toISOString());
+    assert.equal(callArgs[1], '[random-id]');
+    assert.equal(callArgs[2], 'GET');
+    assert.equal(callArgs[3], '/foo');
+    assert.equal(callArgs[4], '?bar=42');
+    assert.equal(callArgs[5], '501');
+    assert.ok(callArgs[6].match(/^\d+ms$/));
+  });
+  it('hideTimestamps: true', async () => {
+    const context = initTestContext();
+    const logFunction = mock.fn(consoleLogNoop);
+    await logRequest({ hideTimestamps: true, logFunction })(context);
+    assert.equal(logFunction.mock.callCount(), 1);
+    const callArgs = logFunction.mock.calls[0].arguments;
+    assert.equal(callArgs[0], '[random-id]');
+    assert.equal(callArgs[1], 'GET');
+    assert.equal(callArgs[2], '/foo');
+    assert.equal(callArgs[3], '?bar=42');
+    assert.equal(callArgs[4], '501');
+    assert.ok(callArgs[5].match(/^\d+ms$/));
+  });
+});
