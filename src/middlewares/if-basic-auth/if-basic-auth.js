2f921998e2839541b9ef5118b112550a35c8b070
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-10-09 00:21:35 +0200

    feat: add if-basic-auth middleware

---
+++ src/middlewares/if-basic-auth/if-basic-auth.js
@@ -0,0 +1,26 @@
+import { getBasicAuth } from '../../lib/authorization.js';
+
+/**
+ * @typedef {import('../../types/hititipi.types.d.ts').HititipiMiddleware} HititipiMiddleware
+ */
+
+/**
+ * @param {string} expectedUser
+ * @param {string} expectedPassword
+ * @param {HititipiMiddleware} middleware
+ * @return {HititipiMiddleware}
+ */
+export function ifBasicAuth(expectedUser, expectedPassword, middleware) {
+  return async (context) => {
+    const { user, password } = getBasicAuth(context.requestHeaders);
+
+    if (user === expectedUser && password === expectedPassword) {
+      return middleware(context);
+    }
+
+    context.responseStatus = 401;
+    context.responseHeaders.set('www-authenticate', 'Basic realm=foo, charset="UTF-8"');
+    context.responseBody = '';
+    return context;
+  };
+}
---
+++ src/middlewares/if-basic-auth/if-basic-auth.md
@@ -0,0 +1,18 @@
+# if-basic-auth middleware
+
+The `if-basic-auth` middleware TODO.
+
+## Import
+
+```js
+import { ifBasicAuth } from 'hititipi/middlewares/if-basic-auth/if-basic-auth.js';
+```
+
+## Examples
+
+TODO
+
+## Links
+
+- [Authorization header on MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization)
+- [Authorization header in RFC 9110](https://httpwg.org/specs/rfc9110.html#field.authorization)
