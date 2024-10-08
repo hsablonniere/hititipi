283bb4ac189e4148c87bb4d75208e8e38a1cfd0a
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-10-09 00:21:35 +0200

    feat: init hititipi common

---
+++ src/hititipi.common.js
@@ -0,0 +1,90 @@
+/**
+ * @typedef {import('./types/hititipi.types.d.ts').HeadersAsObject} HeadersAsObject
+ * @typedef {import('./types/hititipi.types.d.ts').HititipiContext} HititipiContext
+ * @typedef {import('./types/hititipi.types.d.ts').HititipiContextWithResponse} HititipiContextWithResponse
+ */
+
+/**
+ * @param {HeadersAsObject} objectHeaders
+ * @return {Headers}
+ */
+export function toStandardHeaders(objectHeaders) {
+  const headersInit = /** @type {HeadersInit} */ (objectHeaders);
+  return new Headers(headersInit);
+}
+
+/**
+ * @param {Headers} headers
+ * @return {HeadersAsObject}
+ */
+export function toObjectHeaders(headers) {
+  return Object.fromEntries(headers.entries());
+}
+
+const BASE62_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
+
+/**
+ * @param {number} [size]
+ * @return {string}
+ */
+export function getRandomId(size = 10) {
+  const randomBytes = globalThis.crypto.getRandomValues(new Uint8Array(size));
+  return Array.from(randomBytes)
+    .map((byte) => {
+      const randomIndex = byte % BASE62_CHARS.length;
+      return BASE62_CHARS[randomIndex];
+    })
+    .join('');
+}
+
+/**
+ * @param {string|null|undefined} rawRemoteAddress
+ * @param {Headers} headers
+ * @return {Array<string>}
+ */
+export function getRequestIps(rawRemoteAddress, headers) {
+  const xForwaredForHeader = headers.get('x-forwarded-for');
+  if (typeof xForwaredForHeader === 'string') {
+    return xForwaredForHeader.split(',').map((address) => address.trim());
+  }
+
+  if (rawRemoteAddress != null) {
+    // Remove leading ::ffff: IPv4 subnet prefix
+    return [rawRemoteAddress.replace(/^::ffff:/, '')];
+  }
+
+  return [];
+}
+
+/**
+ * @param {string} partialUrl
+ * @param {Headers} headers
+ * @return {URL}
+ */
+export function getRequestUrl(partialUrl, headers) {
+  const protocol = headers.get('x-forwarded-proto') ?? headers.get('x-forwarded-protocol') ?? 'http';
+  const hostname = /** @type {string} */ (headers.get('host'));
+  if (partialUrl.startsWith('http://') || partialUrl.startsWith('https://')) {
+    const url = new URL(partialUrl);
+    url.protocol = protocol;
+    return url;
+  }
+  return new URL(partialUrl, `${protocol}://${hostname}`);
+}
+
+const EMPTY_BODY_STATUSES = [204, 205, 304];
+
+/**
+ * @param {HititipiContext} context
+ * @return {context is HititipiContextWithResponse}
+ */
+export function hasResponseBody(context) {
+  if (
+    context.responseBody != null &&
+    context.responseStatus != null &&
+    !EMPTY_BODY_STATUSES.includes(context.responseStatus)
+  ) {
+    return true;
+  }
+  return false;
+}
---
+++ src/types/hititipi.types.d.ts
@@ -0,0 +1,76 @@
+export type HititipiMethod =
+  | 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'DELETE'
+  | 'PATCH'
+  | 'HEAD'
+  | 'CONNECT'
+  | 'OPTIONS'
+  | 'TRACE'
+  | string;
+
+export type HeadersAsObject = Record<string, string | Array<string> | undefined>;
+
+export interface HititipiContext {
+  requestTimestamp: number;
+  requestId: string;
+  requestIps: Array<string>;
+  requestMethod: HititipiMethod;
+  requestUrl: URL;
+  requestHeaders: Headers;
+  responseStatus?: number;
+  responseHeaders: Headers;
+  responseBody?: string | ArrayBuffer | ReadableStream;
+  responseSize?: number;
+  responseModificationDate?: Date;
+  responseEtag?: Etag;
+}
+
+export type HititipiContextWithResponse = WithRequired<HititipiContext, 'responseBody'>;
+
+export type HititipiMiddleware = (context: HititipiContext) => Promise<HititipiContext>;
+
+export type Etag = WeakEtag | StrongEtag;
+
+export interface WeakEtag {
+  value: string;
+  weak: true;
+}
+
+export interface StrongEtag {
+  value: string;
+  weak: false;
+}
+
+export type UrlParts = HrefPart | HrefSplitParts;
+
+interface HrefPart {
+  href: string;
+}
+
+type HrefSplitParts = (OriginPart | SplitOriginParts) & OtherParts;
+
+interface OriginPart {
+  origin: string;
+}
+
+type SplitOriginParts = ProtocolHostParts | ProtocolHostnamePortParts;
+
+type ProtocolHostParts = {
+  host: string;
+};
+
+type ProtocolHostnamePortParts = {
+  hostname?: string;
+  port?: string;
+};
+
+interface OtherParts {
+  protocol?: string;
+  pathname?: string;
+  search?: string;
+  hash?: string;
+}
+
+type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
