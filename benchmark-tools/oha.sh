4671906a90d99e9b880d10c020e4e3241ded94d8
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-11-02 15:49:38 +0100

    chore: use JSON output for benchmark tools

--- benchmark-tools/oha.sh
+++ benchmark-tools/oha.sh
@@ -7,6 +7,9 @@ oha "http://localhost:$PORT/hello.html?firstname=the%20firstname&lastname=the%20
   --latency-correction \
   --disable-keepalive \
   --wait-ongoing-requests-after-deadline \
+  --no-tui \
+  --disable-color \
+  --json \
   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0' \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8' \
   -H 'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3' \
